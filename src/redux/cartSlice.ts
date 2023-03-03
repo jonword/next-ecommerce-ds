ed9b454900c18d239df674bb0191e8f676a53d61
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-03 17:36:17 -0600

    add cart logic to slice

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSelector, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 import type { Products } from "@prisma/client";
@@ -19,7 +19,35 @@ const initialState: CartState = {
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action: PayloadAction<Products>) => {
+      const item = state.cart.find((x) => x.product.id === action.payload.id);
+
+      state.cart.push({
+        product: action.payload,
+        qty: 1,
+      });
+    },
+
+    removeFromCart: (state, action: PayloadAction<Products>) => {
+      state.cart = state.cart.filter((x) => x.product.id !== action.payload.id);
+    },
+  },
 });
+
+const cart = (state: RootState) => state.cart;
+
+export const totalCartItemSelector = createSelector([cart], (cart) =>
+  cart.reduce((total: number, curr: CartItem) => (total += curr.qty), 0)
+);
+
+export const totalPriceSelector = createSelector([cart], (cart) =>
+  cart.reduce(
+    (total: number, curr: CartItem) => (total += curr.qty * curr.product.price),
+    0
+  )
+);
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
