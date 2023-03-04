0b6f5c2fac15e90c59bdfd730776c5ed3db257f2
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-04 10:32:00 -0600

    fix position on cart indicator

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
             <div
               className={
                 cart.length
-                  ? `quantity-indicator absolute right-7 top-2 flex h-3 w-3 animate-pingOnce items-center justify-center text-gray-100 p-2 rounded-full bg-pink-700 text-xs`
+                  ? `quantity-indicator absolute right-12 top-2 flex h-2 w-2 animate-pingOnce items-center justify-center text-gray-100 p-2 rounded-full bg-pink-700 text-xs`
                   : `hidden`
               }
               key={total}
