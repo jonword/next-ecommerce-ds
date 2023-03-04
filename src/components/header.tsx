e06f6a57d370e9b3c3b0460b73cdaf42f6f8c5cb
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-03 18:20:29 -0600

    add animation to cart indicator

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,13 @@ import { FaBars } from "react-icons/fa";
 import { GiDrumKit } from "react-icons/gi";
 import Link from "next/link";
 import { HiShoppingCart } from "react-icons/hi";
+import { useAppSelector } from "@/redux/hooks";
+import { totalCartItemSelector } from "@/redux/cartSlice";
 
 const Header = () => {
   const [menuActive, setMenuActive] = useState(false);
+  const cart = useAppSelector((state) => state.cart);
+  const total = useAppSelector(totalCartItemSelector);
 
   const toggleMenu = () => setMenuActive(!menuActive);
 
@@ -26,7 +30,17 @@ const Header = () => {
       <nav className="flex relative h-16 justify-end items-center border-b border-slate-200 px-8 md:px-12">
         <div>
           <Link href="/cart">
-            <HiShoppingCart size={30} />
+            <button>
+              <HiShoppingCart size={30} />
+              <div
+                className={
+                  cart.length
+                    ? `quantity-indicator absolute right-12 top-3 flex h-3 w-3 animate-pingOnce items-center justify-center rounded-full bg-red-800`
+                    : `hidden`
+                }
+                key={total}
+              ></div>
+            </button>
           </Link>
         </div>
       </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pingOnce: {
+          "50%": { transform: "scale(2)" },
+          100: { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        pingOnce: "pingOnce 0.5s cubic-bezier(0, 0, 0.2, 1)",
+      },
+    },
   },
   plugins: [],
 };
