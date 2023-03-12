ba640e176f038c150faba33d40702ae7166c2577
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-12 17:30:23 -0500

    apple pay button

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,8 +1,20 @@
-import { NextPage } from "next";
 import React from "react";
+import { FaApple } from "react-icons/fa";
 
-const Checkout: NextPage = () => {
-  return <div>Checkout</div>;
+const Checkout = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-2xl">Checkout</h1>
+      </div>
+      <div className="w-full h-full">
+        <button className="flex items-center bg-gray-900 text-white p-3 px-12 rounded-md">
+          <FaApple size={20} />
+          <p className="text-xl">Pay</p>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
