48235a22e1e53a73c83f72b70a4e07085f3fab2a
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-13 17:53:34 -0500

    start reducer

--- src/redux/filterSlice.ts
+++ src/redux/filterSlice.ts
@@ -2,3 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 import type { Products } from "@prisma/client";
+
+interface Product {
+  product: Products;
+}
+
+interface FilterState {}
+
+const initialState: FilterState = {
+  product: [],
+};
+
+export const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    filterByCategory: (state, action) => {},
+  },
+});
