4c85515828bc8bab16ff1edd695ce784796e2e2f
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-18 11:02:38 -0500

    add logic for empty cart to cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -10,34 +10,38 @@ const Cart = () => {
   const total = useAppSelector(totalCartItemSelector);
   const subtotal = useAppSelector(totalPriceSelector);
   return (
-    <>
-      <h1 className="lg:mx-60 mt-4 text-3xl pl-4">My Cart</h1>
-      <div className="flex flex-col lg:flex-row lg:justify-between gap-8 lg:mx-60 pb-12">
-        <div className="w-full">
-          {cart.map((item) => (
-            <CartPageCard cartItem={item} key={item.product.id} />
-          ))}
+    <div>
+      {cart.length ? (
+        <>
+          <h1 className="lg:mx-60 mt-4 text-3xl pl-4">My Cart</h1>
+          <div className="flex flex-col lg:flex-row lg:justify-between gap-8 lg:mx-60 pb-12">
+            <div className="w-full">
+              {cart.map((item) => (
+                <CartPageCard cartItem={item} key={item.product.id} />
+              ))}
+            </div>
+            <div className="flex flex-col gap-6 p-2 pb-6 w-full lg:max-w-md lg:max-h-52 justify-between">
+              <div className="flex justify-between">
+                <p>Total</p>
+                <p>{formatCurrency(subtotal)}</p>
+              </div>
+              <div className="mt-4 flex justify-start text-gray-500">
+                <p>Taxes and shipping calculated at checkout</p>
+              </div>
+              <div className="flex justify-center">
+                <button className="flex items-center gap-2 bg-gray-900 py-4 px-20 text-white duration-300 hover:bg-gray-700">
+                  <p>CHECK OUT</p>
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          <h1 className="mt-4 pl-4 text-center text-xl">Your cart is empty.</h1>
         </div>
-        <div className="flex flex-col gap-6 p-2 pb-6 w-full lg:max-w-md lg:max-h-52 justify-between">
-          <div className="flex justify-between">
-            <p>Total</p>
-            <p>{formatCurrency(subtotal)}</p>
-          </div>
-          <div className="mt-4 flex justify-start text-gray-500">
-            <p>Taxes and shipping calculated at checkout</p>
-          </div>
-          <div className="flex justify-center">
-            <button className="flex items-center gap-2 bg-gray-900 py-4 px-20 text-white duration-300 hover:bg-gray-700">
-              <p>CHECK OUT</p>
-            </button>
-          </div>
-        </div>
-
-        {/*<div>
-      <h1 className="mt-4 pl-4 text-center text-xl">Your cart is empty.</h1>
-    </div> */}
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
