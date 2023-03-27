86d765506c0ec6cf3e054c264864aef20ee3aea0
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-27 16:41:27 -0500

    create auth api route

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+});
