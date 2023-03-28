1788ab72ecc40df10c969e6f091f58551c3e9ec6
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-28 13:22:36 -0500

    pass in google to onclick sign in

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,19 +4,21 @@ import { useSession, signIn, signOut } from "next-auth/react";
 const Login = () => {
   const { data: session } = useSession();
   return (
-    <>
-      {session ? (
-        <div className="flex flex-col justify-center items-center">
-          <p>Welcome, {session.user?.name}</p>
-          <button onClick={() => signOut()}>Sign out</button>
-        </div>
-      ) : (
-        <div className="flex flex-col justify-center items-center">
-          <p>You are not signed in.</p>
-          <button onClick={() => signIn()}>Sign in</button>
-        </div>
-      )}
-    </>
+    <div className="h-[500px] flex w-full  items-center justify-center">
+      <div className="border h-40 w-96">
+        {session ? (
+          <div className="flex flex-col justify-center items-center">
+            <p>Welcome, {session.user?.name}</p>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <p>You are not signed in.</p>
+            <button onClick={() => signIn("google")}>Sign in</button>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
