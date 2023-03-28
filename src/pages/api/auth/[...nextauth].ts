d61b4cc41ae08ea3c2aadc7086e0319974e1024e
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-03-28 11:14:44 -0500

    add google provider and secret keys in env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,15 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/lib/prisma";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
 });
