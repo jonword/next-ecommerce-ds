0dd9fb1bac2677c64c7bfd719c23216b95f699e1
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-04-04 17:18:57 -0500

    footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="flex fixed bottom-0 h-8 w-full justify-start  px-4 items-center">
+    <footer className="flex h-12 bg-slate-800 w-full justify-start px-4 items-center mt-[100px]">
       <div className="text-slate-300 hover:text-sky-700 hover:cursor-pointer duration-150">
         <a target="_blank" href="https://github.com/jonword/next-ecommerce-ds">
           <FaGithub size={30} />
