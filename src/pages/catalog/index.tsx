8922944fe0c5c4c28264f2321e9bfee14b48fd2a
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-02 14:06:27 -0500

    filter component in catalog page

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,19 +1,41 @@
 import CatalogCard from "@/components/catalogcard";
 import type { Products } from "@prisma/client";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import prisma from "@/lib/prisma";
 import { useState } from "react";
+import { products } from "@/data/data";
+import Filter from "@/components/filter";
+import { useSelector } from "react-redux";
 
 interface Props {
   data: Products[];
 }
 
-const Catalog = ({ data }: Props) => {
-  const [category, setCategory] = useState(null);
+const Catalog = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps> & Props) => {
+  const [category, setCategory] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(data);
+
+  const handleCategoryChange = (category: string) => {
+    setCategory(category);
+    if (category === "") {
+      setFilteredProducts(data);
+    } else {
+      const filtered = data.filter((product) => product.category === category);
+      setFilteredProducts(filtered);
+    }
+  };
 
   return (
     <>
-      <div className="flex pl-6 pt-4 items-start justify-start "></div>
+      <div className="pl-6 pt-4">
+        <Filter
+          products={data}
+          onCategoryChange={handleCategoryChange}
+          selected={category}
+        />
+      </div>
       <div className="m-8" />
       <div className="flex h-full flex-wrap justify-evenly gap-8 pt-2 mb-16">
         {data.map((p) => (
