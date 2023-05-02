70f33cb901723e782657f67f0fac752836b3362e
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-02 14:05:42 -0500

    filter component

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from "react";
+import { Products } from "@prisma/client";
 
-const Filter = ({}) => {
-  return <div>Filter</div>;
+type Props = {
+  products: Products[];
+  selected: string;
+  onCategoryChange: (category: string) => void;
+};
+
+const Filter = ({ products, selected, onCategoryChange }: Props) => {
+  //const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleClick = (category: string) => {
+    onCategoryChange(category);
+  };
+
+  return (
+    <div>
+      <ul className="flex gap-4">
+        <li key="all">
+          <button onClick={() => handleClick("")}>all</button>
+        </li>
+        {[...new Set(products.map((product) => product.category))].map(
+          (category) => (
+            <li key={category}>
+              <button onClick={() => handleClick(category)}>{category}</button>
+            </li>
+          )
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default Filter;
